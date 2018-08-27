94f7653249242b76eebc08b9062052cf207d452a
Grover <41809783+vwhope@users.noreply.github.com>
2018-08-27 14:16:24 -0400

    initial setup files

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+// Javascript for Word-Guess-Game
+//
+// This is just a test to be sure I have .js file linked correctly
+// note to self - remove this before submission
+var  myHeading = document.querySelector("h1");
+myHeading.textContent = "Word-Guess Game";
+
+// start real code here!
+// start with a bank of Farmers Market words
+var wordList = ["apples", "bananas", "cherries" ];
+console.log (wordList);
+// totWords is the maximum number of games the player can guess without repeating  words
+var totWords = wordlist.length;
+console.log(toWords)
+// for each new game, increment the wordList[index] by 1, to go to a new word to guess
